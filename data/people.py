db64a4c4de66f6e0522b605e21ffbb8547541b1f
IamtherealBrian <2638932112@qq.com>
2024-10-08 22:10:34 -0400

    Copy the foundation from the code in class and add create_person data access function

---
+++ data/people.py
@@ -0,0 +1,54 @@
+"""
+This module interfaces to our user data.
+"""
+
+MIN_USER_NAME_LEN = 2
+# fields
+NAME = 'name'
+ROLES = 'roles'
+AFFILIATION = 'affiliation'
+EMAIL = 'email'
+
+TEST_EMAIL = 'ejc369@nyu.edu'
+DEL_EMAIL = 'delete@nyu.edu'
+
+people_dict = {
+    TEST_EMAIL: {
+        NAME: 'Eugene Callahan',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: TEST_EMAIL,
+    },
+    DEL_EMAIL: {
+        NAME: 'Another Person',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: DEL_EMAIL,
+    },
+}
+
+def get_people():
+    """
+    Our contract:
+        - No arguments.
+        - Returns a dictionary of users keyed on user email.
+        - Each user email must be the key for another dictionary.
+    """
+    people = people_dict
+    return people
+
+
+def delete_person(_id):
+    people = get_people()
+    if _id in people:
+        del people[_id]
+        return _id
+    else:
+        return None
+
+def create_person(name: str, affiliation: str, email: str):
+    print("Current people_dict:", people_dict)
+    if email in people_dict:
+        raise ValueError(f'Adding duplicate {email=}')
+    people_dict[email] = {NAME: name, AFFILIATION: affiliation,
+                          EMAIL: email}
