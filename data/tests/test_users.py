2c2e3b7f3f09ea5eb3dee0555bf74408db85e824
Hyan <95455834+Haomingyan@users.noreply.github.com>
2024-10-24 17:06:45 -0400

    delete duplicate file users.py and update test_people.py file

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -1,18 +1,15 @@
-import data.users as usrs
+# import data.users as usrs
 import data.people as ppl
 from data.people import get_person, TEST_EMAIL, NAME, ROLES, AFFILIATION, EMAIL
 
 def test_read():
-    users = usrs.get_users()
-    assert isinstance(users, dict)
-    assert len(users) > 0  # at least one user!
-    for key in users:
-        assert isinstance(key, str)
-        assert len(key) >= usrs.MIN_USER_NAME_LEN
-        user = users[key]
-        assert isinstance(user, dict)
-        assert usrs.LEVEL in user
-        assert isinstance(user[usrs.LEVEL], int)
+    people = ppl.read()
+    assert isinstance(people, dict)
+    assert len(people) > 0
+    # check for string IDs:
+    for _id, person in people.items():
+        assert isinstance(_id, str)
+        assert ppl.NAME in person
 
 def test_delete_person():
     # Test deleting an existing person
