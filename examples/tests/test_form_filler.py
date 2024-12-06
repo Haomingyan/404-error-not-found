7d5b77b6173525c17c8f6c536949db8f8a40923e
Daniel Liang <100398055+DanielLiangAjj@users.noreply.github.com>
2024-12-05 20:32:12 -0500

    Added test_get_query_fid_names and test_form_default_value

--- examples/tests/test_form_filler.py
+++ examples/tests/test_form_filler.py
@@ -18,3 +18,22 @@ def test_get_form_descr():
 @patch('examples.form_filler.get_input', return_value='Y')
 def test_form(mock_get_input):
     assert isinstance(ff.form(ff.TEST_FLD_DESCRIPS), dict)
+
+def test_get_query_fld_names():
+    ret = ff.get_query_fld_names(ff.TEST_FLD_DESCRIPS)
+    assert isinstance(ret, list)
+    assert ff.TEST_FLD in ret
+
+
+def test_form_default_value():
+    fld_descrips = [
+        {
+            ff.FLD_NM: 'default_test_field',
+            ff.DEFAULT: 'default_value',
+            ff.PARAM_TYPE: ff.QUERY_STR,
+            ff.QSTN: 'What is the default value?'
+        }
+    ]
+    with patch('examples.form_filler.get_input', return_value='') as mock_get_input:
+        ret = ff.form(fld_descrips)
+        assert ret['default_test_field'] == 'default_value'
