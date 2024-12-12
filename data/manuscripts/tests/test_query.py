d75ace7bc09ad326af77b5a4b73ffe090bae8863
Hyan <95455834+Haomingyan@users.noreply.github.com>
2024-12-11 22:01:15 -0500

    add test for not valid state and valid action in test_query.py

--- data/manuscripts/tests/test_query.py
+++ data/manuscripts/tests/test_query.py
@@ -1,6 +1,25 @@
 import data.manuscripts.query as mqry
 
+import random
+
+
+def gen_random_not_valid_str()-> str:
+    BIG_NUM = 10_000_000_000
+    big_int = random.randint(0, BIG_NUM)
+    big_int += BIG_NUM
+    bad_str = str(big_int)
+
+
 def test_is_valid_state():
     for state in mqry.get_states():
         assert mqry.is_valid_state(state)
-    
+
+
+def test_is_not_valid_state():
+    for state in mqry.get_states():
+        assert not mqry.is_valid_state(gen_random_not_valid_str)
+
+
+def test_is_valid_action():
+    for action in mqry.get_actions():
+        assert mqry.is_valid_action(action)
