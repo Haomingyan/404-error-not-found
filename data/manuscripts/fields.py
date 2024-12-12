8c84488b29da27ebc254fef4ae941213a1711d08
Daniel Liang <100398055+DanielLiangAjj@users.noreply.github.com>
2024-12-11 22:20:01 -0500

    Added more functions to fields.py

--- data/manuscripts/fields.py
+++ data/manuscripts/fields.py
@@ -17,3 +17,16 @@ FIELDS = {
 
 def get_flds() -> dict:
     return FIELDS
+
+def get_fld_names() -> list:
+    return FIELDS.keys()
+
+def get_disp_name(fld_nm: str) -> dict:
+    fld = FIELDS.get(fld_nm, '')
+    return fld[DISP_NAME]  # should we use get() here?
+
+def main():
+    print(f'{get_flds()=}')
+
+if __name__ == '__main__':
+    main()
