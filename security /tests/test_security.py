1e25528899250456ad127b38e51d549945601ba8
samuelwang <zw3079@nyu.edu>
2025-02-11 20:37:56 -0800

    added test_read_feature function

--- security /tests/test_security.py
+++ security /tests/test_security.py
@@ -7,3 +7,27 @@ def test_read():
     for feature in recs:
         assert isinstance(feature, str)
         assert len(feature) > 0
+
+
+def test_read_feature_people():
+    """
+    Test that read_feature can retrieve the PEOPLE feature
+    and that it has the expected structure.
+    """
+    # Make sure data is loaded first
+    sec.read()
+
+    # Now call read_feature
+    people_data = sec.read_feature(sec.PEOPLE)
+
+    # Check the result is a dict
+    assert isinstance(people_data, dict), "PEOPLE feature should be a dictionary"
+
+    # Example of checking if it has a CREATE key or something else
+    assert sec.CREATE in people_data, "PEOPLE feature should have a CREATE key"
+    assert isinstance(people_data[sec.CREATE], dict), "CREATE should map to a dict"
+
+    # Optionally, you can check the user_list or checks subfields
+    create_data = people_data[sec.CREATE]
+    assert sec.USER_LIST in create_data, "CREATE should have a USER_LIST"
+    assert sec.CHECKS in create_data, "CREATE should have a CHECKS dictionary"
