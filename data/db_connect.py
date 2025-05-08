e445f6d8e8d9af4523e022a3e9e5837080e7e9e7
IamtherealBrian <2638932112@qq.com>
2025-05-07 22:03:16 -0400

    update db password without hardcoded

--- data/db_connect.py
+++ data/db_connect.py
@@ -28,8 +28,7 @@ def connect_db():
     if client is None:  # not connected yet!
         print("Setting client because it is None.")
         if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
-            # password = os.environ.get("GAME_MONGO_PW")
-            password = "goodpassword"
+            password = os.environ.get("GAME_MONGO_PW")
             if not password:
                 raise ValueError('You must set MONGO_PW to your password '
                                  + 'to use Mongo in the cloud.')
--- local.sh
+++ local.sh
@@ -4,7 +4,7 @@ export FLASK_ENV=development
 export PROJ_DIR=$PWD
 export DEBUG=1
 export CLOUD_MONGO=1   # 1 is cloud MongoDB, 0 is local MongoDB
-export GAME_MONGO_PW=404-error-not-found
+export GAME_MONGO_PW=goodpassword
 
 # run our server locally:
 PYTHONPATH=$(pwd):$PYTHONPATH
